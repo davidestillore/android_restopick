09671bb61d76a04d8ebf4bf355554252be9e8170
David Estillore <dcestillore@gmail.com>
2020-09-22 21:03:49 +0800

    First Commit

---
+++ app/src/main/java/com/example/restopick/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.restopick
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Pizzahut", "Mcdonalds", "Jollibee", "KFC", "Shakeys")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+            println(foodList)
+        }
+    }
+}
